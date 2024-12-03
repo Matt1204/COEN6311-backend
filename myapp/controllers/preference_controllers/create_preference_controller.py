c8abbcfac0e7ee522d6626d8acc15d8f9ef419b7
mazihan <mazihan@zlketang.com>
2024-12-02 23:53:56 -0500

    Hardcoding 'max_hours_per_shift', 'hours_per_week', and 'hours_per_shift'. Check duplicate data when create_request and create_preference. Updating database file.

--- myapp/controllers/preference_controllers/create_preference_controller.py
+++ myapp/controllers/preference_controllers/create_preference_controller.py
@@ -1,3 +1,8 @@
+"""
+    'max_hours_per_shift' and 'hours_per_week' in shift_preference table
+    is hardcoded to 8 and 40 due to Algorithm Limitation
+"""
+
 from ...config import get_db_connection
 from flask import jsonify
 import json
@@ -44,6 +49,9 @@ def create_preference(pref_data):
     #             print(f"{field}->{pref_data[field]} ({type(pref_data[field])})")
 
     try:
+
+        cursor = conn.cursor()
+
         nurse_id = pref_data.get("nurse_id")
         time_of_day = pref_data.get("time_of_day")
         start_date = pref_data.get("start_date")
@@ -52,18 +60,38 @@ def create_preference(pref_data):
         max_hours_per_shift = pref_data.get("max_hours_per_shift")
         preferred_week_days = json.dumps(pref_data.get("preferred_week_days"))
         hospitals_ranking = json.dumps(pref_data.get("hospitals_ranking"))
+
+        check_query = """
+            SELECT *
+            FROM shift_preference
+            WHERE nurse_id = %s AND start_date = %s
+        """
+        cursor.execute(check_query, [nurse_id, start_date])
+        check_result = cursor.fetchone()
+        if check_result:
+            return (
+                jsonify(
+                    {
+                        "error": "client-side issue",
+                        "message": "Preference already exists",
+                    }
+                ),
+                400,
+            )
+
         fields = "nurse_id, time_of_day, start_date, end_date, hours_per_week, preferred_week_days, max_hours_per_shift, hospitals_ranking"
         values = "%s, %s, %s, %s, %s, %s, %s, %s"
-        cursor = conn.cursor()
         query = f"INSERT INTO shift_preference ({fields}) VALUES ({values})"
         params = [
             nurse_id,
             time_of_day,
             start_date,
             end_date,
-            hours_per_week,
+            # hours_per_week,
+            40,  # "hours_per_week" in "shift_preference" table is hardcoded to 40 due to Algorithm Limitation
             preferred_week_days,
-            max_hours_per_shift,
+            # max_hours_per_shift,
+            8,  # "max_hours_per_shift" in "shift_preference" table is hardcoded to 8 due to Algorithm Limitation
             hospitals_ranking,
         ]
         cursor.execute(query, params)
